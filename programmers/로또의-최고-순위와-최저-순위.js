495de16960f5c42e48e85d18d9811aca07b9fee3
unknown <junsang.yu3@gmail.com>
2021-05-04 10:20:30 +0900

    코드리뷰 개선점

--- programmers/로또의-최고-순위와-최저-순위.js
+++ programmers/로또의-최고-순위와-최저-순위.js
@@ -1,8 +1,8 @@
 function solution(lottos, win_nums) {
-  var answer = lottos.reduce((acc, val) => {
+  let answer = lottos.reduce((acc, val) => {
     return acc + win_nums.includes(val);
   }, 0);
-  let zeros = lottos.filter((val) => { return val === 0 }).length;
+  let zeros = lottos.filter((val) => val === 0).length;
   let min = 7 - answer;
   let max = 7 - answer - zeros;
   return [(max > 6) ? 6 : max, (min > 6) ? 6 : min];
