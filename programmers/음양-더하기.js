3e306a53c069312bf62459443895b6c90a29acae
unknown <junsang.yu3@gmail.com>
2021-05-04 12:58:16 +0900

    rm

--- programmers/음양-더하기.js
+++
@@ -1,6 +0,0 @@
-function solution(absolutes, signs) {
-  var answer = absolutes.reduce((acc, val, idx) => {
-    return acc + (signs[idx] ? (val) : (-val));
-  }, 0);
-  return answer;
-}
--- programmers/제일-작은-수-제거하기.js
+++
@@ -1,5 +0,0 @@
-function solution(arr) {
-  arr = arr.filter((val) => { return val != Math.min(...arr) });
-  if (arr.length === 0) return [-1];
-  return arr;
-}
