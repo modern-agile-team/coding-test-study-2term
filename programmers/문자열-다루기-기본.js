4ae1be134afbb12b8bf6683c658ef275cac69da3
lovelyoch"ohchanghoon <dhckdgns3@naver.com>
2021-05-06 17:16:01 +0900

    ohchanghoon: 문자열 다루기 기본 풀이 수정

--- programmers/문자열-다루기-기본.js
+++ programmers/문자열-다루기-기본.js
@@ -1,10 +1,8 @@
 function solution(s) {
     
-    const len = s.length;
-    console.log(len)
+    let answer = false;
+    let temp = Number(s)
     
-    return (len === 4 || len === 6) && !isNaN(s) ? true : false;
+    return (s.length === 4 || s.length === 6) && temp === parseInt(s) ? true : false;
     
-    // 테스트 11번에서 통과하지않는데 구글링해도 잘모르겠습니다.
-    // 테스트 11은 지수형식일때라고 합니다
-}
+};
