3c73eb001f3e17a968f0c7ab4f744a4a3647f45d
minsoonki <alstnsrl98@gmail.com>
2021-05-07 12:10:24 +0900

    로또 제출합니다

---
+++ programmers/로또.js
@@ -0,0 +1,21 @@
+function solution(lottos, win_nums) {
+    
+    let [zCnt, wCnt] = [0, 0];
+    let [best, worst] = [0, 0];
+    
+    for (let i = 0; i < 6; i++) {
+        if(lottos[i] === 0) zCnt += 1;
+        
+        for (let j = 0; j < 6; j++) {
+            if (lottos[i] === win_nums[j]) wCnt += 1;
+        }
+    }
+    
+    best = Math.abs(wCnt + zCnt - 7);
+    
+    if (zCnt === 6) worst = 6;
+    else worst = Math.abs (wCnt - 7);
+    
+    if (wCnt === 0 && zCnt === 0) return [6, 6];
+    return [best, worst];
+}
