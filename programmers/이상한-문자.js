314d689236fe2ec0bda13fb3cc28559cc0309d86
Min <alstnsrl98@gmail.com>
2021-05-10 12:02:21 +0900

    이상한 문자

--- programmers/이상한-문자.js
+++ programmers/이상한-문자.js
@@ -1,14 +1,13 @@
 function solution(s) {
 
     s = s.split(" ");
-    const sWrap = [...s];               //const sWrap = [...(s.split(" "))];  이것도 가능하더라구요. 이렇게 하면 줄을 줄일수 있지만 가독성이 좋은것 같지는 않아서 어떤식으로 표현하는게 나을지 여쭤보고싶습니다.
-    const result = new Array;
-    const len = sWrap.length;
-    
-    for (let i = 0; i < len; i++) {
-        for(let j = 0; j < sWrap[i].length; j++) {
+    const sWrap = [...s];   
+    //const sWrap = [...(s.split(" "))];  이것도 가능하더라구요. 이렇게 하면 줄을 줄일수 있지만 가독성이 좋은것 같지는 않아서 어떤식으로 표현하는게 나을지 여쭤보고싶습니다.            
+    const result = new Array; 
+    for (let i in sWrap) {
+        for(let j in sWrap[i]) {
             !(j % 2) ? result.push(sWrap[i][j].toUpperCase()) : result.push(sWrap[i][j].toLowerCase());
-        } if (i < len - 1) result.push(" ");
+        } if (i < sWrap.length - 1) result.push(" ");
     }    
     return result.join("");
 }
