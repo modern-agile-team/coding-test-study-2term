54f6a9e198b85930ca68b4a68267a774b47b06e6
unknown <kimjisu3817@gmail.com>
2021-05-10 17:12:29 +0900

    kimjisu: 음양 더하기 풀이 수정

---
+++ programmers2/음양-더하기(수정).js
@@ -0,0 +1,8 @@
+function solution(absolutes, signs) {
+    var sum = 0;
+    for(let i = 0; i < absolutes.length; i++) {
+        if(signs[i]) sum += absolutes[i];
+        else sum -= absolutes[i];
+    }
+    return sum;
+}
