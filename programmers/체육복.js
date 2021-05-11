390fc03ce93eebced2f372a1f4c235c61cd1060d
lovelyoch"ohchanghoon <dhckdgns3@naver.com>
2021-05-11 15:33:17 +0900

    ohchanghoon: 체육복 풀이

---
+++ programmers/체육복.js
@@ -0,0 +1,29 @@
+function solution(n, lost, reserve) {
+    let answer = 0;
+    let haveNum = n - lost.length; 
+
+    for (let i = 0; i < lost.length; i++) {
+        if (reserve.indexOf(lost[i]) !== -1) {
+            reserve.splice(reserve.indexOf(lost[i]), 1);
+            lost.splice(i, 1);
+            haveNum ++;
+            i--;				
+        }
+    }
+	
+    for (let num of lost) {
+        let frontCheck = reserve.indexOf(num - 1)
+        let behindCheck = reserve.indexOf(num + 1)
+		
+        if (frontCheck !== -1) {
+            reserve.splice(frontCheck, 1);
+            haveNum++;
+        } else if (behindCheck !== -1) {
+            reserve.splice(behindCheck, 1);
+            haveNum++;
+        }
+    }
+
+    answer = haveNum;
+    return answer;
+}
