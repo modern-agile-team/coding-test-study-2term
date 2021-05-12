2b8707fe6bde0aea039a24f27d58a968cd458924
Bsfla <baebeomsu77@gmail.com>
2021-05-12 16:33:03 +0900

    baebumsoo : 신규아이디추천 풀이

--- programmers/신규-아이디-추천.js
+++ programmers/신규-아이디-추천.js
@@ -1,13 +1,12 @@
 function solution(new_id) {
-    
-   new_id = new_id
+    new_id = new_id
         .toLowerCase()
         .replace(/[^\w\.\-]/g, '')
         .replace(/[\.]{2,}/g, '.')
         .replace(/^\./, '')
         .replace(/\.$/, '');
-    if (!new_id)  new_id = "a";
     
+    if (!new_id)  new_id = "a";
     if (new_id.length >= 16) {
         new_id = new_id
             .slice(0, 15)
