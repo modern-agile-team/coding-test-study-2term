2e490d5af9010543e1c81c3dbe21690ff3a435e7
Soonki <alstnsrl98@gmail.com>
2021-06-10 14:09:15 +0900

    비밀지도 -민순기

---
+++ programmers/비밀지도.js
@@ -0,0 +1,24 @@
+function solution(n, arr1, arr2) {
+    
+    let result = [];
+    const output = [];
+    for (let i = 0; i < n; i++) {
+        arr1[i] = arr1[i].toString(2);
+        arr2[i] = arr2[i].toString(2);
+        if (arr1[i].length < n) {
+            while (arr1[i].length < n) arr1[i] = 0 + arr1[i];
+        }
+        if (arr2[i].length < n) {
+            while (arr2[i].length < n) arr2[i] = 0 + arr2[i];
+        }
+        for (let j = 0; j < n; j++) {
+            if (arr1[i][j] === '0' && arr2[i][j] === '0') result.push(' ');
+            if (arr1[i][j] === '1' || arr2[i][j] === '1') result.push('#')
+        }
+    }
+    result = result.join('');
+    for (let i = 0; i < n; i++) {
+        output.push(result.substr(i * n, n))
+    }
+    return output;
+}
