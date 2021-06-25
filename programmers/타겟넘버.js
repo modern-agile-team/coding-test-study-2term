e815fc3649c9e1f9b1fe894e903a0bb378e047a1
seohyunsim <ssh4563@naver.com>
2021-06-25 10:24:00 +0900

    simseohyun: 타겟넘버-풀이

---
+++ programmers/타겟넘버.js
@@ -0,0 +1,14 @@
+function solution(numbers, target) {
+    let answer = 0;
+    
+    function dfs(index, sum) {
+        if (index === numbers.length) {
+            if (sum === target) answer++;
+            return;
+        }
+        dfs(index + 1, sum + numbers[index]);
+        dfs(index + 1, sum - numbers[index]);
+    }
+    dfs(0, 0);
+    return answer;
+}
