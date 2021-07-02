8a4d4eb62f1558b8680771a6cf93fefa00e645f5
rugaheedori <qufslarkgml@gmail.com>
2021-07-02 17:51:22 +0900

    gahee : 하노이의 탑

--- programmers/하노이의-탑.js
+++ programmers/하노이의-탑.js
@@ -4,9 +4,9 @@ function solution(n) {
     function hanoi(n, start, finish, middle) {
         if (n === 1) answer.push([start, finish]);
         else {
-            hanoi(n-1, start, middle, finish);
+            hanoi(n - 1, start, middle, finish);
             answer.push([start, finish]);
-            hanoi(n-1, middle, finish, start);
+            hanoi(n - 1, middle, finish, start);
         }
     }
 
