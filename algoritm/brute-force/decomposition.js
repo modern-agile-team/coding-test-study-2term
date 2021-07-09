db4b2ee9381834d1ee498be1062fccd16f4af2e8
Soonki <alstnsrl98@gmail.com>
2021-07-09 17:25:28 +0900

    let str -> const str로 수정

--- algoritm/brute-force/decomposition.js
+++ algoritm/brute-force/decomposition.js
@@ -2,7 +2,7 @@ const N = parseInt(require("fs").readFileSync("/dev/stdin").toString());
 function decomposition(N) {
   let result = 0;
   for (let i = 1; i < N + 1; i++) {
-    let str = String(i);
+    const str = String(i);
     let sum = 0;
     for (let j = 0; j < str.length; j++) {
       sum += Number(str[j]);
