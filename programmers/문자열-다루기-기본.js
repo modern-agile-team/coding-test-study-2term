7d3f4a76aa81d5ff7a87d66df77cbd56e6739cc0
Bsfla <baebeomsu77@gmail.com>
2021-07-09 16:45:09 +0900

    삭제

--- programmers/문자열-다루기-기본.js
+++
@@ -1,6 +0,0 @@
-function solution(s) {
-  let result = parseInt(s);
-  
-  if ((s.length === 4 || s.length === 6) && s == result) return true;
-  return false;
-}
--- programmers/수박수박수박수박수박수?.js
+++
@@ -1,8 +0,0 @@
-function solution(n) {
-    let answer = ""
-        for( let i = 1; i < n+1; i++){
-            if( i % 2 === 0) answer += '박';
-            else answer += '수';
-        }
-    return answer;
-}
