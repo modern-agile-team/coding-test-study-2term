d6757a5d9284ef8eaafde37236b9f208710adadf
Soonki <alstnsrl98@gmail.com>
2021-07-16 17:57:15 +0900

    변수명 수정

--- algoritm/sort/H-index.js
+++ algoritm/sort/H-index.js
@@ -3,7 +3,7 @@ function solution(citations) {
   let result = 0;
 
   //   citations.sort();        메서드
-  //   bubbleSort(citations);   버블정렬 사용자 정의 함수
+  //   bubbleSort(citations);   버블소트 사용자 정의 함수
 
   for (let h = 0; h < citations[n - 1]; h++) {
     if (
@@ -16,12 +16,12 @@ function solution(citations) {
   return result;
 }
 
-function bubbleSort(numbers) {
-  const n = numbers.length;
+function bubbleSort(numArr) {
+  const n = numArr.length;
   for (let i = 0; i < n - 1; i++) {
     for (let j = 0; j < n - i; j++) {
-      if (numbers[j + 1] < numbers[j]) {
-        [numbers[j], numbers[j + 1]] = [numbers[j + 1], numbers[j]];
+      if (numArr[j + 1] < numArr[j]) {
+        [numArr[j], numArr[j + 1]] = [numArr[j + 1], numArr[j]];
       }
     }
   }
--- algoritm/sort/biggestNum.js
+++ algoritm/sort/biggestNum.js
@@ -2,16 +2,15 @@ function solution(numbers) {
   let result = numbers.map((number) => number.toString());
   bubbleSort(result);
   result = result.filter((el) => el !== undefined).join("");
-
   return parseInt(result) === 0 ? "0" : result;
 }
 
-function bubbleSort(numbers) {
-  const n = numbers.length;
+function bubbleSort(numArr) {
+  const n = numArr.length;
   for (let i = 0; i < n - 1; i++) {
     for (let j = 0; j < n - i; j++) {
-      if (numbers[j + 1] + numbers[j] > numbers[j] + numbers[j + 1]) {
-        [numbers[j], numbers[j + 1]] = [numbers[j + 1], numbers[j]];
+      if (numArr[j + 1] + numArr[j] > numArr[j] + numArr[j + 1]) {
+        [numArr[j], numArr[j + 1]] = [numArr[j + 1], numArr[j]];
       }
     }
   }
