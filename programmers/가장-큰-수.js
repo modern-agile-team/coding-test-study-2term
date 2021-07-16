b343d1d873750b8f37c5c83911e8dccffa1c07de
unknown <junsang.yu3@gmail.com>
2021-07-16 16:49:38 +0900

    합병정렬 추가

--- programmers/가장-큰-수.js
+++ programmers/가장-큰-수.js
@@ -13,9 +13,28 @@ function bubble(arr) {
       if (compareNumbers(arr[j], arr[j + 1]))
         [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
 }
+function merge(a, b) {
+  let arr = [];
+  while (a.length && b.length) {
+    if (compareNumbers(a[0], b[0])) {
+      arr.push(b.shift());
+    } else {
+      arr.push(a.shift())
+    }
+  }
+  arr = [...arr, ...a, ...b];
+  return arr;
+}
+function divide(arr) {
+  if (arr.length < 2) return arr;
+  const left = divide(arr.slice(0, Math.floor(arr.length / 2)));
+  const right = divide(arr.slice(Math.floor(arr.length / 2)));
+  return merge(left, right);
+}
 
 function solution(numbers) {
-  numbers.sort((a, b) => (compareNumbers(a, b) ? 1 : -1));
+  // numbers.sort((a, b) => (compareNumbers(a, b) ? 1 : -1));
+  numbers = divide(numbers);
   if (numbers.every((value) => value === 0)) {
     return '0';
   }
