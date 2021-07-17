523dcc185cedfe7a8ed664d7b5a1560d0d844206
rugaheedori <qufslarkgml@gmail.com>
2021-07-18 08:27:59 +0900

    gahee : 가장 큰 수(sort사용x , 시간초과o)

--- programmers/소수-찾기.js
+++ programmers/소수-찾기.js
@@ -15,7 +15,7 @@ function combine(splitedNums, N) {
 function analysis(num) {
     if (num === 1 || num === 0) return false;
     if (num === 2) return true;
-    for (let i=2; i < Math.sqrt(num) + 1; i++) {
+    for (let i = 2; i < Math.sqrt(num) + 1; i++) {
         if (num % i === 0) return false;
     }
     return true;
