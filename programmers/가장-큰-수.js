f39cc9915e081d447c3169f4b29f03fe092364c8
rugaheedori <qufslarkgml@gmail.com>
2021-07-18 08:35:30 +0900

    gahee: 가장 큰 수 (sort 사용x, 시간초과o)

--- programmers/가장-큰-수.js
+++ programmers/가장-큰-수.js
@@ -5,9 +5,10 @@ function solution(numbers) {
             const num2 = String(numbers[j]);
             if (Number(num1 + num2) < Number(num2 + num1)) {
                 [numbers[i], numbers[j]] = [numbers[j], numbers[i]];
-                console.log(numbers);
             }
         }
     }
     return numbers.join("");
 }
+const numbers = [3, 30, 34, 5, 9];
+console.log("결과",solution(numbers));
