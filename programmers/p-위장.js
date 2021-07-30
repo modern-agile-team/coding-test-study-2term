b95bbfbcdbc2fb735d9e918e95385c55fb352cd2
rugaheedori <qufslarkgml@gmail.com>
2021-07-30 12:49:23 +0900

    gahee : 위장

---
+++ programmers/p-위장.js
@@ -0,0 +1,11 @@
+function solution(clothes) {
+    const answer = {};
+
+    clothes.forEach(x => {
+        x[1] in answer ? answer[x[1]] += 1 : answer[x[1]] = 1;
+    })
+    const result = Object.values(answer).reduce((base, currValue) => {
+        return base * (currValue + 1);
+    }, 1);
+    return result - 1;
+}
